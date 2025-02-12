6f412de8de15e33c4b63325de20809ed69231504
sabbirsolid <sabbir.designer21@gmail.com>
2025-02-12 15:52:23 +0600

    final

--- index.js
+++ index.js
@@ -92,38 +92,6 @@ async function run() {
       const result = await blogsCollection.insertOne(blog);
       res.send(result);
     });
-   
-    // app.get("/blogs", async (req, res) => {
-    //   const page = parseInt(req.query.page);
-    //   const size = parseInt(req.query.size);
-    //   const search = req.query.search || ""; // Get the search query, default to empty string if not provided
-    //   const category = req.query.category || ""; // Get the category, default to empty string if not provided
-
-    //   let filter = {};
-
-    //   // If there's a search query, filter the blogs by title (or other fields like content)
-    //   if (search) {
-    //     filter.title = { $regex: search, $options: "i" }; // Case-insensitive search by title
-    //   }
-
-    //   // If a category is selected, filter by category
-    //   if (category) {
-    //     filter.category = category;
-    //   }
-
-    //   try {
-    //     const result = await blogsCollection
-    //       .find(filter)
-    //       .skip(page * size)
-    //       .limit(size)
-    //       .toArray();
-    //     res.send(result);
-    //   } catch (error) {
-    //     res
-    //       .status(500)
-    //       .send({ message: "Error fetching blogs", error: error.message });
-    //   }
-    // });
 
     // testing
     app.get("/blogs", async (req, res) => {
@@ -133,22 +101,22 @@ async function run() {
       const category = req.query.category || "";
       const sortField = req.query.sortField || "";
       const sortOrder = req.query.sortOrder || "asc";
-    
+
       let filter = {};
-    
+
       if (search) {
         filter.title = { $regex: search, $options: "i" };
       }
-    
+
       if (category) {
         filter.category = category;
       }
-    
+
       let sortOptions = {};
       if (sortField) {
         sortOptions[sortField] = sortOrder === "asc" ? 1 : -1;
       }
-    
+
       try {
         const result = await blogsCollection
           .find(filter)
